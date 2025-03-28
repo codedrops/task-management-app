8457c84d2ae79d2192a706a886c7388b7db287b6
Rohith Ramesh <33133438+codedrops@users.noreply.github.com>
2025-03-28 16:51:34 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the Node.js Express App!');
+});
+
+export default app;
